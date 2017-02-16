83b52c7a054a8893091e6d2a1f215ea0b539f675
BeVC <Bernard_Van_Couwenberghe@hotmail.com>
2017-02-16 21:22:02 +0100

    pantryitems to inmemorydataservice

--- src/in-memory-data.service.ts
+++ src/in-memory-data.service.ts
@@ -3,9 +3,15 @@ import { InMemoryDbService } from "angular-in-memory-web-api";
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         let users = [
-            { id: "1a", firstName: 'John', lastName: "Doe", email: "spam@email.com", pantryId: "", createdAt: new Date(), updatedAt: null, deletedAt: null, password: "Enter1" },
-            { id: "1a", firstName: 'Bernard', lastName: "VC", email: "spam2@email.com", pantryId: "1b", createdAt: new Date(), updatedAt: null, deletedAt: null, password: "Azert2" }
+            { id: "u1", firstName: 'John', lastName: "Doe", email: "spam@email.com", pantryId: "ab", createdAt: new Date(), updatedAt: null, deletedAt: null, password: "Enter1" },
+            { id: "u2", firstName: 'Bernard', lastName: "VC", email: "spam2@email.com", pantryId: "1b", createdAt: new Date(), updatedAt: null, deletedAt: null, password: "Azert2" }
         ];
-        return { users };
+
+        let pantryItems = [
+            { id: "pi1", name: "spaghettisaus", pantryId: "1b" },
+            { id: "pi2", name: "stoofpot", pantryId: "1b" }
+        ]
+
+        return { users, pantryItems };
     }
 }
